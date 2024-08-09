4e65c38e2059858a12c45d20980043426943890e
Océane TARDY <tardy.oceane42@gmail.com>
2024-08-09 11:59:50 +0200

    🎉 Initialision du projet
    Mise en place du projet Meeting Minder 2

---
+++ src/main/java/com/example/MeetingMinder/MeetingMinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeetingMinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingMinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingMinderApplication.class, args);
+	}
+
+}
