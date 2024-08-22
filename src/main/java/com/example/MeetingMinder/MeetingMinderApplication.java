eefce7a64ba212cc48a02f6656b51c192bfc474e
Romain LANTELME <lantelme.romain@gmail.com>
2024-08-22 19:04:13 +0200

    [+] Ajout des validations pour les rôles et amélioration de la gestion des erreurs

--- src/main/java/com/example/MeetingMinder/MeetingMinderApplication.java
+++ src/main/java/com/example/MeetingMinder/MeetingMinderApplication.java
@@ -2,8 +2,6 @@ package com.example.MeetingMinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MeetingMinderApplication {
@@ -11,10 +9,4 @@ public class MeetingMinderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingMinderApplication.class, args);
 	}
-
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-
 }
